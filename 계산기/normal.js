a9651b3c113614193067e557de1d26397a7a17cc
In-chijun <clwnssla1@bible.ac.kr>
2022-06-04 14:18:11 +0900

    6/4 14:18

--- 계산기/normal.js
+++ 계산기/normal.js
@@ -1,101 +1,87 @@
-var oneValue = function(value){
+var oneValue = function (value) {
     this.value = value
 }
-var twoValue = function(value1, value2){
+var twoValue = function (value1, value2) {
     this.value1 = value1
     this.value2 = value2
 }
 
-const OneValue = (function() {
-    function OneValue() {
+const setCalculation = (function () {
+    function setCalculation() {
         this.strategy = null;
     }
-    OneValue.prototype.setStrategy = function(strategy) {
+    setCalculation.prototype.setStrategy = function (strategy) {
         this.strategy = strategy;
     }
-    OneValue.prototype.doAlgorithm = function(value) {
-        this.strategy.algorithm(value);
+    setCalculation.prototype.doAlgorithm = function () {
+        this.strategy.algorithm.apply(null, args);
     }
-    return OneValue;
-})();
-const TwoValue = (function() {
-    function TwoValue() {
-        this.strategy = null;
-    }
-    TwoValue.prototype.setStrategy = function(strategy) {
-        this.strategy = strategy;
-    }
-    TwoValue.prototype.doAlgorithm = function(value) {
-        this.strategy.algorithm(value);
-    }
-    return TwoValue;
+    return setCalculation;
 })();
 
 // 값 1개
-const pi = (function() {
-    function pi() {}
-    pi.prototype.algorithm = function(value) {    
+const pi = (function () {
+    function pi() { }
+    pi.prototype.algorithm = function (value) {
         console.log(Math.PI * value);
     };
     return pi;
 })();
-const absolute = (function() {
-    function absolute() {}
-    absolute.prototype.algorithm = function(value) {    
+const absolute = (function () {
+    function absolute() { }
+    absolute.prototype.algorithm = function (value) {
         console.log(Math.abs(value));
     };
     return absolute;
 })();
-const squareRoot = (function() {
-    function squareRoot() {}
-    squareRoot.prototype.algorithm = function(value) {    
+const squareRoot = (function () {
+    function squareRoot() { }
+    squareRoot.prototype.algorithm = function (value) {
         console.log(Math.sqrt(value));
     };
     return squareRoot;
 })();
-const factorial = (function() {
-    function factorial() {}
-    factorial.prototype.algorithm = function(value) {    
+const factorial = (function () {
+    function factorial() { }
+    factorial.prototype.algorithm = function (value) {
         result = 1;
-        for (value; value > 1; value--){
-                    result *= value;
-                }
-                console.log(result);
+        for (value; value > 1; value--) result *= value;
+        console.log(result);
     };
     return factorial;
 })();
 // 값 2개
-const plus = (function() {
-    function plus() {}
-    plus.prototype.algorithm = function(value1, value2) {    
+const plus = (function () {
+    function plus() { }
+    plus.prototype.algorithm = function (value1, value2) {
         console.log(value1 + value2);
     };
     return plus;
 })();
-const minus = (function() {
-    function minus() {}
-    minus.prototype.algorithm = function(value1, value2) {    
+const minus = (function () {
+    function minus() { }
+    minus.prototype.algorithm = function (value1, value2) {
         console.log(value1 - value2);
     };
     return minus;
 })();
-const multiplication = (function() {
-    function multiplication() {}
-    multiplication.prototype.algorithm = function(value1, value2) {    
+const multiplication = (function () {
+    function multiplication() { }
+    multiplication.prototype.algorithm = function (value1, value2) {
         console.log(value1 * value2);
     };
     return multiplication;
 })();
-const division = (function() {
-    function division() {}
-    division.prototype.algorithm = function(value1, value2) {    
+const division = (function () {
+    function division() { }
+    division.prototype.algorithm = function (value1, value2) {
         console.log(value1 / value2);
     };
     return division;
 })();
-const exponentiation = (function() {
-    function exponentiation() {}
-    exponentiation.prototype.algorithm = function(value1, value2) {    
+const exponentiation = (function () {
+    function exponentiation() { }
+    exponentiation.prototype.algorithm = function (value1, value2) {
         console.log(Math.pow(value1, value2));
     };
     return exponentiation;
@@ -109,70 +95,47 @@ let oneValue = new OneValue();
 // oneValue.doAlgorithm(3);
 // // 팩토리얼
 
-// oneValue.setStrategy(new absolute());
-// oneValue.doAlgorithm(-5);
-
-// const set = {
-//     setOperation(operation) {
-//       return oneValue.setStrategy(new operation());
-//     }
-// }
-// const dooneValue = {
-//     doExecute(number) {
-//       return oneValue.doAlgorithm(number);
-//     }
-// }
-
-const facto = Object.assign({}, OneValue.setStrategy(new factorial()), OneValue.doAlgorithm(3)); // 메서드 팩토리 패턴이 맞는지
-
 // const OneMethod = Operation => {
 //     let orderItem = createConstructor(Operation);
-    
+
 //     OneValue.setStrategy(orderItem);
 //     OneValue.doAlgorithm(3);
-  
+
 //     return orderItem;
-//   };
-  
-//   const oneFactory = Operation => {
-//     let orderItem;
-      
-//       if (Operation === 'coffee') {
-//         orderItem = new CoffeeOrder();
-//       } else if (Operation === 'bread') {
-//         orderItem = new BreadOrder();
-//       } else if (Operation === 'juice') {
-//         orderItem = new JuiceOrder();
-//       }
-      
-//       return orderItem;
-//   }
+// };
 
-var Section = function(){}
-var PersonalSection = new Section();
-var AlbumSection = new Section();
-var PatentSection = new Section();
-var PublicationSection = new Section();
+// const oneFactory = operation => {
+//     return {
+        // 'coffee': new CoffeeOrder(),
+        // 'bread': new BreadOrder(),
+        // 'juice': new JuiceOrder()
+//     }[operation];
+// }
 
-var SNS = function(){
-    SNS.prototype.sections = {};
-    SNS.prototype.createProfile = function(){};
-    SNS.prototype.getSections = function(){
-        return this.sections;
+var Caculation = (function() {
+    function Caculation(value1, value2) {
+      this.value1 = value1;
+      this.value2 = value2 || 0;
     }
-    SNS.prototype.addSection = function(section){
-        this.sections.push(section)
-    }
-}
-
-const Facebook = new SNS();
-Facebook.prototype.createProfile = function(){
-    addSection(new PersonalSection());
-    addSection(new PatentSection());
-    addSection(new PublicationSection());
-}
-const Twitter = new SNS();
-Twitter.prototype.createProfile = function(){
-    addSection(new PersonalSection());
-    addSection(new AlbumSection());
-}
+    Caculation.prototype.action = function(value) {
+        setCalculation.doAlgorithm(value);
+    };
+    return Caculation;
+  })();
+  var makeCalculation = operation => {
+        return {
+            'factorial': new factorial(),
+            'pi': new pi(),
+            'squareRoot': new squareRoot(),
+            'absolute': new absolute(),
+            'plus': new plus(),
+            'multiplication': new multiplication(),
+            'exponentiation': new exponentiation(),
+            'minus': new minus(),
+            'division': new division(),
+        }[operation];
+      };
+var galbaLegion = makeLegion('galba').setAdjutant(['otho', 'vindex', 'vitellius']).setArmy(8000).build();
+var rufusLegion = makeLegion('rufus').setArmy(10000).build();
+console.log(galbaLegion)
+console.log(rufusLegion.attack(galbaLegion));
--- 계산기/practice.js
+++ 계산기/practice.js
@@ -1,31 +1,31 @@
-var Section = function(){}
-var PersonalSection = new Section();
-var AlbumSection = new Section();
-var PatentSection = new Section();
-var PublicationSection = new Section();
-
-const SNS = function(){
-    SNS.prototype.sections = {};
-    SNS.prototype.createProfile = function(){};
-    SNS.prototype.getSections = function(){
-        return this.sections;
+  var Caculation = (function() {
+    function Caculation(value1, value2) {
+      this.value1 = value1;
+      this.value2 = value2 || 0;
     }
-    SNS.prototype.addSection = function(section){
-        this.sections.push(section)
+    Caculation.prototype.excute = function(value) {
+        oneValue.doAlgorithm(value);
+    };
+    return Caculation;
+  })();
+  var makeLegion = function(Operation) {
+    var adjutants = null;
+    var army = null;
+    return {
+      setAdjutant: function(list) {
+        adjutants = list;
+        return this;
+      },
+      setArmy: function(number) {
+        army = number;
+        return this;
+      },
+      build: function() {
+        return new Legion(leader, adjutants, army);
+      }
     }
-}
-
-const Facebook = new SNS();
-Facebook.prototype.createProfile = function(){
-    addSection(new PersonalSection());
-    addSection(new PatentSection());
-    addSection(new PublicationSection());
-}
-const Twitter = new SNS();
-Twitter.prototype.createProfile = function(){
-    addSection(new PersonalSection());
-    addSection(new AlbumSection());
-}
-var sns = new Facebook();
-sns.createProfile();
-console.log(sns.getSections());
+  };
+var galbaLegion = makeLegion('galba').setAdjutant(['otho', 'vindex', 'vitellius']).setArmy(8000).build();
+var rufusLegion = makeLegion('rufus').setArmy(10000).build();
+console.log(galbaLegion)
+console.log(rufusLegion.attack(galbaLegion));
