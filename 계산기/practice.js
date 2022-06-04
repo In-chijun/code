e3571d85a5ffc81e3ca2deb102beed2c1ec1c112
Daniel <clwnssla1@bible.ac.kr>
2022-06-04 14:20:21 +0900

    Update practice.js

--- 계산기/practice.js
+++ 계산기/practice.js
@@ -29,3 +29,36 @@ var galbaLegion = makeLegion('galba').setAdjutant(['otho', 'vindex', 'vitellius'
 var rufusLegion = makeLegion('rufus').setArmy(10000).build();
 console.log(galbaLegion)
 console.log(rufusLegion.attack(galbaLegion));
+
+//팩토리 메서드 패턴
+var Section = function(){}
+var PersonalSection = new Section();
+var AlbumSection = new Section();
+var PatentSection = new Section();
+var PublicationSection = new Section();
+
+const SNS = function(){
+    SNS.prototype.sections = {};
+    SNS.prototype.createProfile = function(){};
+    SNS.prototype.getSections = function(){
+        return this.sections;
+    }
+    SNS.prototype.addSection = function(section){
+        this.sections.push(section)
+    }
+}
+
+const Facebook = new SNS();
+Facebook.prototype.createProfile = function(){
+    addSection(new PersonalSection());
+    addSection(new PatentSection());
+    addSection(new PublicationSection());
+}
+const Twitter = new SNS();
+Twitter.prototype.createProfile = function(){
+    addSection(new PersonalSection());
+    addSection(new AlbumSection());
+}
+var sns = new Facebook();
+sns.createProfile();
+console.log(sns.getSections());
